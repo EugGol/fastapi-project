018db61b9681dae0573180d6e1c7f4b8019ddbe9
EugGol <dragonborn1613@gmail.com>
2025-06-20 19:15:35 +0300

    create facilities

---
+++ src/repositories/facilities.py
@@ -0,0 +1,9 @@
+
+from src.schemas.facilities import Facility
+from src.models.facilities import FacilitiesOrm
+from src.repositories.base import BaseRepository
+
+class FacilitiesRepository(BaseRepository):
+    model = FacilitiesOrm
+    schema = Facility
+    
