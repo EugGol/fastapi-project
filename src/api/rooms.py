f82276215354f5015106a4d90480095a2845b1dd
EugGol <dragonborn1613@gmail.com>
2025-06-21 14:27:19 +0300

    add update facilities

--- src/api/rooms.py
+++ src/api/rooms.py
@@ -1,5 +1,7 @@
+from datetime import date
 from fastapi import APIRouter, Body, HTTPException
 
+from src.schemas.facilities import RoomFacilityAdd
 from src.api.dependencies import DBDep
 from src.schemas.rooms import RoomAdd, RoomAddRequest, RoomPatchRequest, RoomPatch
 
@@ -7,8 +9,8 @@ router = APIRouter(prefix="/hotels", tags=["Номера"])
 
 
 @router.get("/{hotel_id}/rooms")
-async def get_rooms(hotel_id: int, db: DBDep):
-    return await db.rooms.get_filtered(hotel_id=hotel_id)
+async def get_rooms(hotel_id: int, db: DBDep, date_from : date, date_to : date):
+    return await db.rooms.get_filtered_by_time(hotel_id=hotel_id, date_from=date_from, date_to=date_to)
 
 
 @router.get("{hotel_id}/rooms/{room_id}", description="Получение номера по ID")
@@ -26,16 +28,17 @@ async def create_room(
                 "summary": "Люкс",
                 "value": {
                     "title": "Номер на двоих",
-                    "description": None,
+                    "description": 'string',
                     "price": 4500,
                     "quantity": 5,
+                    "facilities_ids": None,
                 },
             },
             "2": {
                 "summary": "Эконом",
                 "value": {
                     "title": "Номер на одного",
-                    "description": None,
+                    "description": 'string',
                     "price": 1500,
                     "quantity": 7,
                 },
@@ -45,8 +48,12 @@ async def create_room(
 ):
     _room_data = RoomAdd(hotel_id=hotel_id, **room_data.model_dump())
     room = await db.rooms.add(_room_data)
+
+    rooms_facilities_data = [RoomFacilityAdd(room_id=room.id, facility_id=f_id) for f_id in room_data.facilities_ids]
+    await db.rooms_facilities.add_bulk(rooms_facilities_data)
     await db.commit()
     return {"status": "OK", "data": room}
+
 
 
 @router.put("/{hotel_id}/rooms/{room_id}")
@@ -55,6 +62,8 @@ async def update_room(
 ):
     _room_data = RoomAdd(hotel_id=hotel_id, **room_data.model_dump())
     await db.rooms.edit(_room_data, id=room_id, hotel_id=hotel_id)
+    rooms_facilities_data = {RoomFacilityAdd(room_id=room_id, facility_id=f_id) for f_id in room_data.facilities_ids}
+    await db.rooms_facilities.update_facilities(rooms_facilities_data)
     await db.commit()
     return {"status": "OK"}
 
@@ -67,6 +76,9 @@ async def update_patc_room(
         hotel_id=hotel_id, **room_data.model_dump(exclude_unset=True)
     )
     await db.rooms.edit(_room_data, exclude_unset=True, id=room_id, hotel_id=hotel_id)
+    if room_data.facilities_ids:
+        rooms_facilities_data = {RoomFacilityAdd(room_id=room_id, facility_id=f_id) for f_id in room_data.facilities_ids}
+        await db.rooms_facilities.update_facilities(rooms_facilities_data)
     await db.commit()
     return {"status": "OK"}
 
--- src/repositories/facilities.py
+++ src/repositories/facilities.py
@@ -1,9 +1,36 @@
 
-from src.schemas.facilities import Facility
-from src.models.facilities import FacilitiesOrm
+from sqlalchemy import delete, insert, select
+
+from src.schemas.facilities import Facility, RoomFacilityAdd
+from src.models.facilities import FacilitiesOrm, RoomsFacilitiesOrm
 from src.repositories.base import BaseRepository
 
 class FacilitiesRepository(BaseRepository):
     model = FacilitiesOrm
     schema = Facility
     
+
+class RoomsFacilitiesRepository(BaseRepository):
+    model = RoomsFacilitiesOrm
+    schema = RoomFacilityAdd
+
+    async def update_facilities(self, room_id: int, data: set[BaseModel]) -> None: # type: ignore
+        tmp = select(self.model.facility_id).filter(self.model.room_id == room_id)
+        res = await self.session.execute(tmp)
+        current_facilities = set(res.scalars().all())
+        delete_stmt = current_facilities - data
+        insert_stmt = data - current_facilities
+
+        if delete_stmt:
+            delete_stmt = delete(self.model).filter(self.model.room_id == room_id).filter(self.model.facility_id.in_(delete_stmt))
+            await self.session.execute(delete_stmt)
+        if insert_stmt:
+            insert_stmt = insert(self.model).values([{"room_id": room_id, "facility_id": f_id} for f_id in insert_stmt])
+            await self.session.execute(insert_stmt)
+
+        await self.session.commit()
+
+
+
+
+        
