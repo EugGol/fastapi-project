22d721943156ebdfabfbb40130f56ad0c3bdf66a
EugGol <dragonborn1613@gmail.com>
2025-08-02 18:11:57 +0300

    create api test for get hotels

---
+++ test/integration_tests/hotels/test_api.py
@@ -0,0 +1,9 @@
+async def test_get_hotels(async_client):
+    response = await async_client.get(
+        "/hotels",
+        params={
+            "date_from":"2025-01-01",
+            "date_to":"2025-01-15"
+        }
+    )
+    assert response.status_code == 200
