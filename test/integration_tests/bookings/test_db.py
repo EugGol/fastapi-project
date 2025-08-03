c9b9b8c261a95e36b5583aab77f8e0e0f8551e52
EugGol <dragonborn1613@gmail.com>
2025-08-03 12:32:35 +0300

    fix test_db

--- test/integration_tests/bookings/test_db.py
+++ test/integration_tests/bookings/test_db.py
@@ -8,35 +8,37 @@ async def test_add_booking_crud(db):
     booking_data = BookingAdd(
         room_id=room_id,
         user_id=user_id,
-        date_from=date(year=2025, month=1, day=1),
-        date_to=date(year=2025, month=1, day=15),
+        date_from=date(year=2024, month=1, day=1),
+        date_to=date(year=2024, month=1, day=15),
         price=100,
     )
-    await db.bookings.add(booking_data)
+    
+    new_booking = await db.bookings.add(booking_data)
+    # получить эту бронь и убедиться что она есть
+    booking = await db.bookings.get_one_or_none(id=new_booking.id)
+    assert booking
+    assert booking.id == new_booking.id
+    assert booking.room_id == new_booking.room_id
+    assert booking.user_id == new_booking.user_id
+    # а еще можно вот так разом сравнить все параметры
+    assert booking.model_dump(exclude={"id"}) == booking_data.model_dump()
 
-    booking = (await db.bookings.get_all())[0]
-    booking_get_one = await db.bookings.get_one_or_none(room_id=room_id)
-
-    assert booking == booking_get_one
-    assert booking.room_id == room_id
-    assert booking.date_from == date(year=2025, month=1, day=1)
-
-    booking_id = (await db.bookings.get_all())[0].id
-
-    booking_data = BookingAdd(
-        room_id=room_id,
+    # обновить бронь
+    updated_date = date(year=2024, month=8, day=25)
+    update_booking_data = BookingAdd(
         user_id=user_id,
-        date_from=date(year=2024, month=1, day=10),
-        date_to=date(year=2025, month=1, day=15),
-        price=10000,
+        room_id=room_id,
+        date_from=date(year=2024, month=8, day=10),
+        date_to=updated_date,
+        price=100,
     )
-    await db.bookings.edit(data=booking_data, id=booking_id)
-    booking = (await db.bookings.get_all())[0]
+    await db.bookings.edit(update_booking_data, id=new_booking.id)
+    updated_booking = await db.bookings.get_one_or_none(id=new_booking.id)
+    assert updated_booking
+    assert updated_booking.id == new_booking.id
+    assert updated_booking.date_to == updated_date
 
-    assert booking.date_from == date(year=2024, month=1, day=10)
-    assert booking.price == 10000
-
-    await db.bookings.delete(id=booking_id)
-    assert (await db.bookings.get_all()) == []
-
-    await db.rollback()
+    # удалить бронь
+    await db.bookings.delete(id=new_booking.id)
+    booking = await db.bookings.get_one_or_none(id=new_booking.id)
+    assert not booking
