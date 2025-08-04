95e7b7dd846980c29cee48fac91e818e96e1ece8
EugGol <dragonborn1613@gmail.com>
2025-08-04 18:38:08 +0300

    fix test_api for bookings.py

--- test/integration_tests/bookings/test_api.py
+++ test/integration_tests/bookings/test_api.py
@@ -1,10 +1,9 @@
 import pytest
 import pytest_asyncio
 
+from test.conftest import get_db_null_poll
 from src.utils.db_manger import DBManager
 from src.database import async_session_maker_null_poll
-
-
 
 
 @pytest.mark.parametrize(
@@ -45,27 +44,28 @@ async def test_add_booking(
         assert "data" in res
 
 
-
 @pytest_asyncio.fixture(scope="session")
 async def delete_all_bookings():
-    async with DBManager(session=async_session_maker_null_poll) as db_:
-        await db_.bookings.delete()
-        await db_.commit()
+    async for _db in get_db_null_poll():
+        await _db.bookings.delete()
+        await _db.commit()
 
 
-@pytest.mark.parametrize( "room_id, date_from, date_to, me_booking", [
-    (1, "2025-01-01", "2025-01-15", 1),
-    (1, "2025-01-01", "2025-01-15", 2),
-    (1, "2025-01-01", "2025-01-15", 3),
-    ]
+@pytest.mark.parametrize(
+    "room_id, date_from, date_to, me_booking",
+    [
+        (1, "2025-01-01", "2025-01-15", 1),
+        (1, "2025-01-01", "2025-01-15", 2),
+        (1, "2025-01-01", "2025-01-15", 3),
+    ],
 )
 async def test_add_and_get_bookings(
-    delete_all_bookings,
-    authenticated_ac,
     room_id,
     date_from,
     date_to,
-    me_booking,    
+    me_booking,
+    delete_all_bookings,
+    authenticated_ac,
 ):
     responce_add_booking = await authenticated_ac.post(
         "/bookings",
@@ -79,4 +79,3 @@ async def test_add_and_get_bookings(
     assert responce_add_booking.status_code == 200
     assert responce_my_booking.status_code == 200
     assert me_booking == len(responce_my_booking.json())
-
